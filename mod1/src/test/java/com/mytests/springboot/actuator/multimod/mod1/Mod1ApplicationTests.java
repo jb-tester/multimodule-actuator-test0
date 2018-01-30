dc0cc6247450497d2311f0cc0b2e0ee4cf1c3d8b
irina-petrovskaya <Irina.Petrovskaya@jetbrains.com>
2018-01-30 17:38:59 +0300

    add test methods for mod1

--- mod1/src/test/java/com/mytests/springboot/actuator/multimod/mod1/Mod1ApplicationTests.java
+++ mod1/src/test/java/com/mytests/springboot/actuator/multimod/mod1/Mod1ApplicationTests.java
@@ -1,7 +1,9 @@
 package com.mytests.springboot.actuator.multimod.mod1;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -9,8 +11,17 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class Mod1ApplicationTests {
 
+	@Autowired
+	private AlarmIndicator alarmIndicator;
+
 	@Test
 	public void contextLoads() {
+		System.out.println("passed!!!");
 	}
 
+	@Test
+	public void testAlarmIndicator() {
+		String ai = alarmIndicator.health().getStatus().toString();
+		Assert.assertEquals("UP",ai );
+	}
 }
